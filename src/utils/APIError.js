d6a04f334a71394c242bc1966c5dcd8b9d4da213
KartikGoel2629 <goelkartik597171@gmail.com>
2026-03-05 11:21:03 +0530

    utils code

---
+++ src/utils/APIError.js
@@ -0,0 +1,23 @@
+class APIError extends Error {
+    constructor(
+        statusCode, 
+        message="Something went wrong",
+        error=[],
+        stack=""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.success=false
+        this.data=null
+        this.message=message
+        this.error=error
+
+        if(stack){
+            this.stack=stack
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export {APIError}
